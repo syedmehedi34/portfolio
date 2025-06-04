f4f3a13bc4838d28140bc55d48bef124ee8c1127
syedmehedi34 <syedmeehdi34@gmail.com>
2025-06-04 09:02:31 +0600

    added the color scema

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { motion } from "framer-motion";
 import { Github, Linkedin, Mail, Facebook, FileDown } from "lucide-react";
-import { FaWhatsapp } from "react-icons/fa6";
+import { FaFacebookF, FaLinkedinIn, FaWhatsapp } from "react-icons/fa6";
 import photo from "/my-photo.jpeg";
 import { Typewriter } from "react-simple-typewriter";
 import backgroundImage from "../assets/background-image.jpg";
@@ -9,7 +10,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="flex items-center pt-32 pb-10 relative min-h-screen"
+      className="flex items-center pt-28 pb-10 relative min-h-screen"
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundAttachment: "fixed",
@@ -18,7 +19,7 @@ const Hero = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="absolute inset-0 bg-blue-950 opacity-70"></div>
+      <div className="absolute inset-0 bg-cyan-950 opacity-70"></div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <motion.div
@@ -29,70 +30,62 @@ const Hero = () => {
             <h2 className="text-4xl font-bold text-white mb-4">
               MERN Stack Developer
             </h2>
-            <h3 className="text-5xl font-bold text-purple-400 mb-4">
+            <h3 className="text-5xl font-bold text-cyan-600 mb-4">
               <Typewriter
                 words={["Syed Mehedi Hasan"]}
-                loop={false}
-                cursor
-                cursorStyle="_"
-                typeSpeed={70}
-                deleteSpeed={50}
-                delaySpeed={4000}
+                // loop={false}
+                // cursor
+                // cursorStyle="_"
+                // typeSpeed={70}
+                // deleteSpeed={50}
+                // delaySpeed={4000}
               />
             </h3>
-            <p className="text-white mb-8">
+            <p className="text-white mb-8 font-semibold">
               A passionate and dedicated developer from Bangladesh, committed to
               creating innovative and responsive web applications. Constantly
               learning and evolving with the latest technologies.
             </p>
             <div className="flex space-x-6 mb-8">
-              <a
-                href="https://linkedin.com/in/syedmehedi34"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white hover:text-purple-300"
-              >
-                <Linkedin className="w-7 h-7" />
-              </a>
-              <a
-                href="https://github.com/syedmehedi34"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white hover:text-purple-300"
-              >
-                <Github className="w-7 h-7" />
-              </a>
-              <a
-                href="https://facebook.com/syedmehedi34"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white hover:text-purple-300"
-              >
-                <Facebook className="w-7 h-7" />
-              </a>
-              <a
-                href="mailto:syedmehedi34@gmail.com"
-                className="text-white hover:text-purple-300"
-              >
-                <Mail className="w-7 h-7" />
-              </a>
-              <a
-                href="https://wa.me/+8801731771438"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white hover:text-purple-300"
-              >
-                <FaWhatsapp className="w-7 h-7" />
-              </a>
+              {[
+                {
+                  href: "https://linkedin.com/in/syedmehedi34",
+                  Icon: FaLinkedinIn,
+                },
+                { href: "https://github.com/syedmehedi34", Icon: Github },
+                // {
+                //   href: "https://facebook.com/syedmehedi34",
+                //   Icon: FaFacebookF,
+                // },
+                { href: "mailto:syedmehedi34@gmail.com", Icon: Mail },
+                { href: "https://wa.me/+8801731771438", Icon: FaWhatsapp },
+              ].map(({ href, Icon }, index) => (
+                <div
+                  key={index}
+                  className="border border-cyan-600 p-2.5 rounded-full transition duration-700 ease-in-out hover:shadow-[0_0_15px_2px_rgba(255,255,255,0.8)]"
+                  style={{ transitionProperty: "box-shadow, border-color" }}
+                >
+                  <div className="p-2.5 rounded-full bg-cyan-600 transition-colors duration-700 ease-in-out">
+                    <a
+                      href={href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-white"
+                    >
+                      <Icon className="w-6 h-6" />
+                    </a>
+                  </div>
+                </div>
+              ))}
             </div>
-            <a
+            {/* <a
               href="./my_resume.pdf"
               target="_blank"
-              className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-cyan-600 hover:bg-cyan-700"
             >
               <FileDown className="w-5 h-5 mr-2" />
               Download Resume
-            </a>
+            </a> */}
           </motion.div>
           <motion.div
             initial={{ opacity: 0, x: 50 }}
@@ -103,7 +96,7 @@ const Hero = () => {
             <img
               src={photo}
               alt="Syed Mehedi Hasan"
-              className="rounded-full w-[350px] h-[350px] md:w-[450px] md:h-[450px] mx-auto shadow-lg object-cover border-4 border-purple-300"
+              className="rounded-full w-[350px] h-[350px] md:w-[450px] md:h-[450px] mx-auto shadow-lg object-cover border-3 border-cyan-600"
             />
           </motion.div>
         </div>
